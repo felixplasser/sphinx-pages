1775582a037a802ee0745647a39f9f391256c1ea
Felix Plasser <f.plasser@lboro.ac.uk>
2025-07-19 18:54:54 +0100

    use TheoDORE docs

--- docs/index.rst
+++ docs/index.rst
@@ -1,22 +1,31 @@
-.. matilda documentation master file
+.. theodore documentation master file, created by
+   sphinx-quickstart on Thu May 13 13:39:18 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
 
-MATILDA
-=======
+TheoDORE
+====================================
 
-Welcome to the MATILDA documentation!
+Welcome to the TheoDORE 3 documentation!
 
 .. toctree::
    :maxdepth: 1
    :caption: Contents:
 
    installation
-   int_coor
-   alignment
-   vibcoup
-   util
+   usage
+   input_generation
+   transition_density_analysis
+   difference_density_analysis
+   chemical_shielding
+   plotting
+   orbitals_and_densities
+   program_specific_information
+   keywords
 
 If you have any questions,
-post an `issue on github <https://github.com/felixplasser/matilda/issues>`_,
-or send me an email at f.plasser(at)lboro.ac.uk.
+write to the `discussion forum <https://sourceforge.net/p/theodore-qc/discussion/>`_,
+post an `issue on github <https://github.com/felixplasser/theodore-qc/issues>`_
+or send me an email to f.plasser(at)lboro.ac.uk.
 
 .. include:: ../README.rst
--- matilda-docs/conf.py
+++ matilda-docs/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'TheoDORE'
+project = 'MATILDA'
 copyright = '2025, Felix Plasser'
 author = 'Felix Plasser'
 
 # The full version, including alpha/beta/rc tags
-release = '3.2'
+release = '0.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,17 +31,16 @@ release = '3.2'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [ "sphinx_rtd_theme",
-              "colt.sphinx",
 ]
 
-html_logo='_static/theodore.png'
+#html_logo='_static/theodore.png'
 
-bibtex_bibfiles = ['refs.bib']
-bibtex_encoding = 'latin'
-bibtex_default_style = 'unsrt'
+#bibtex_bibfiles = ['refs.bib']
+#bibtex_encoding = 'latin'
+#bibtex_default_style = 'unsrt'
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -63,4 +62,4 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
